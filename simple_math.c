0a76ac29d42f4054e314ad8fe14e7fa2f329e21f
silvia di piazza <s650793@studenti.unige.it>
2025-02-27 13:27:00 +0100

    first version  of simple.math - no test

---
+++ simple_math.c
@@ -0,0 +1,14 @@
+/** \file simple_math.h 
+	\brief a toy exemple of math library 
+	\author Diego El mejor 
+	
+*/
+
+
+#inclsude #simple_math.h#
+
+float mean (int a, int b){
+	return (a+b)/2;
+}
+
+#endif 
---
+++ simple_math.h
@@ -0,0 +1,13 @@
+/** \file simple_math.h 
+	\brief a toy exemple of math library 
+	\author Diego El mejor 
+	
+*/
+
+#ifndef _SIMPLEMATH_
+#define _SIMPLEMATH_
+
+float mean (int a, int b);
+
+
+#endif 
