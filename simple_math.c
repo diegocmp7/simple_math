d727658e356dc0e9bfcfe12586582fe0fdcdc493
diegocmp7 <s650793@studenti.unige.it>
2025-02-27 13:55:41 +0100

    first version  of simple.math - no test1

--- simple_math.c
+++ simple_math.c
@@ -1,14 +1,1 @@
-/** \file simple_math.h 
-	\brief a toy exemple of math library 
-	\author Diego El mejor 
-	
-*/
-
-
-#inclsude #simple_math.h#
-
-float mean (int a, int b){
-	return (a+b)/2;
-}
-
-#endif 
+/** \file simple_math.h 	\brief a toy exemple of math library 	\author Diego El mejor 	*/#include "simple_math.h"float mean(int a, int b){		return (a+b)/2;}
