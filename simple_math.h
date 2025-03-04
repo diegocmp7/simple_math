02fc1fabb6dca70389ec5540baffad20ee992907
diegocmp7 <s650793@studenti.unige.it>
2025-03-04 12:17:17 +0100

    Tested complex number

--- simple_math.h
+++ simple_math.h
@@ -1,12 +1,31 @@
 /** \file simple_math.h
 	\brief a toy example of math library
-	\author Paolo Gastaldo
-  
+	\author Diego Campos
 */
 
 #ifndef _SIMPLEMATH_ 
 #define _SIMPLEMATH_
 
-float mean (int a, int b);
+typedef struct complex_int {
+	int re; 
+	int imm;
+} cx_int_t;
+
+typedef struct complex_float {
+	float re; 
+	float imm;
+} cx_float_t;
+
+float mean(int a, int b);
+cx_float_t cx_mean (cx_int_t a,cx_int_t b);
+
+int max (int a, int b);
 
 #endif
+
+
+
+
+
+
+
