42a028dc4b238f94cdf68c53c645ebb57b3a3884
diegocmp7 <s650793@studenti.unige.it>
2025-03-04 19:46:25 +0100

    hotfix

--- simple_math.c
+++ simple_math.c
@@ -6,17 +6,6 @@
 */
 #include "simple_math.h"
 
-cx_float_t cx_mean (cx_int_t a,cx_int_t b) {
-	
-	cx_float_t average;
-	
-	average.re = mean(a.re, b.re);
-	average.imm = mean(a.imm, b.imm);
-	
-	return average;
-	
-}
-
 
 float mean(int a, int b) {
 	return (a+b)/2;
