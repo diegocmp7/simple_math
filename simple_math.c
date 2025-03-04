ef7fa4ef9738e8cf8b7911cc0712c7f768edab56
diegocmp7 <s650793@studenti.unige.it>
2025-03-04 12:28:34 +0100

    hotfix of mean fuction

--- simple_math.c
+++ simple_math.c
@@ -1,11 +1,24 @@
+
 /** \file simple_math.c
 	\brief definition of the functions 
-	\author Paolo Gastaldo
+	\author Diego Campos 
   
 */
+#include "simple_math.h"
 
 #include "simple_math.h"
 
 float mean(int a, int b) {
-	return (a+b)/2;
+	return (a+b)/2.0;
 }
+
+int max (int a, int b) {
+	
+	if (a > b) 
+		return a; 
+	return b;
+}
+
+
+
+
--- simple_math.h
+++ simple_math.h
@@ -1,12 +1,23 @@
 /** \file simple_math.h
 	\brief a toy example of math library
-	\author Paolo Gastaldo
-  
+	\author Diego Campos
 */
 
 #ifndef _SIMPLEMATH_ 
 #define _SIMPLEMATH_
 
-float mean (int a, int b);
+
+
+float mean(int a, int b);
+
+
+int max (int a, int b);
 
 #endif
+
+
+
+
+
+
+
