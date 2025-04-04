336092371a99e5a932831f5351ebfbd746029050
diegocmp7 <s650793@studenti.unige.it>
2025-04-04 15:45:56 +0200

    a

--- simple_math.c
+++ simple_math.c
@@ -6,20 +6,8 @@
 
 #include "simple_math.h"
 
-cx_float_t cx_mean (cx_int_t a,cx_int_t b) {
-	
-	cx_float_t average;
-	
-	average.re = mean(a.re,b.re);
-	average.imm = mean(a.imm,b.imm);
-	
-	return average;
-	
-}
-
-
 float mean(int a, int b) {
-	return (a+b)/2;
+	return (a+b)/2.0;
 }
 
 int max (int a, int b) {
